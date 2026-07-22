ab0a6ae8eb4f1c5be4370bce3fd7605632e3b8a3
Churchill-S <2754582811@qq.com>
2026-07-22 12:18:20 +0800

    基于YOLO11的四种mc方块识别模型

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 from ultralytics import YOLO
 
 # 加载训练好的模型（.pt 文件）
-model = YOLO('D:/Churchill/TongJi/主要课程/工程智能基础/机器人小项目/my_data/runs/detect/train/weights/best_openvino_model/')
+model = YOLO('weights文件夹之前的完整路径/weights/best_openvino_model/') #如果直接复制Windows给的路径要注意把"\"改为"/"，同时记得删掉多余的双引号
 
 # 对一张图片进行预测
-results = model('D:/Churchill/TongJi/主要课程/工程智能基础/机器人小项目/my_data/runs/detect/images/blue.jpg')  # 改成你的图片路径
+results = model('完整图片路径')  # 改成你的图片路径
 
 # 显示结果
 results[0].show()  # 会弹窗显示标注后的图片
